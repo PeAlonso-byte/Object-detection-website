ea08c6e45c060fb9c59225c8f66bb563ff323e5f
Pedro Alonso <PedroAlonso@usal.es>
2024-01-20 20:06:30 +0100

    Added image classification page

---
+++ object-detection-web/app/image-classification/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+type Props = {}
+
+const ImageClassification = (props: Props) => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default ImageClassification
